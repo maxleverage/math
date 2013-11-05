49f972ffb7ca65c666eece040e0488f41e476bf5
Henry Wang <henrywang7@gmail.com>
2013-11-05 16:33:42 +0000

    Lowest common denominator of product of 2 digit curious fractions

---
+++ curious_fractions.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import math as m
+import time
+
+""" Curious Fraction Checker """
+
+def curious_fraction(numerator, denominator):
+	if numerator < denominator:
+		initial_result = float(numerator) / denominator
+		numerator = str(numerator)
+		denominator = str(denominator)
+		for i in numerator:
+			for j in denominator:
+				numerator_cancel = [x for x in numerator if x != i]
+				denominator_cancel = [x for x in denominator if x != j]
+				if (float(j) > 0) and (numerator_cancel == denominator_cancel) and (float(i)/float(j) == initial_result):
+					output_result = str(i) + '/' + str(j)
+					return output_result
+					break
+				else: 
+					output_result = 0
+		return output_result
+	else: return 0
+
+
+""" Iterate until all curious fractions are found """
+""" There are four curious fractions """
+
+start = time.time()
+exclusion_list = [10,20,30,40,50,60,70,80,90]
+exclusion_list_2 = [22,33,44,55,66,77,88,99]
+numerator_list = range(10,99)
+numerator_list = [x for x in numerator_list if x not in exclusion_list]
+numerator_list = [x for x in numerator_list if x not in exclusion_list_2]
+denominator_list = range(11,99)
+denominator_list = [x for x in denominator_list if x not in exclusion_list]
+denominator_list = [x for x in numerator_list if x not in exclusion_list_2]
+fraction_result = []
+for i in numerator_list:
+	for j in denominator_list:
+		if type(curious_fraction(i,j)) == str:
+			fraction_result.append(str(i) + '/' + str(j))
+
+""" Multiply Fraction Result """
+
+numerator_product = 1
+for element in fraction_result:
+	numerator_product *= int(element[0:2])
+
+
+denominator_product = 1
+for element in fraction_result:
+	denominator_product *= int(element[3:])
+
+""" GCD """
+
+def gcd(a,b):
+	while b:
+		a, b = b, a % b
+	return a
+
+""" Lowest Common Denominator """
+
+lowest_common_denominator = denominator_product / gcd(denominator_product, numerator_product)
+finish_time = time.time() - start
+print "The solution is %i and it took %f seconds to find the solution" % (lowest_common_denominator, finish_time)
+
